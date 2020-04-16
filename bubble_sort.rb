f41f9230996c481a3967812ec9ce4dbeb32cfa42
karthi07 <itkarthi02@gmail.com>
2020-04-16 14:22:48 +0530

    add sort by

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -16,9 +16,22 @@ def bubble(array)
   array
 end
 
+def bubble_sort_by(arr)
+    for i in 1..arr.length()
+         for j in (0..arr.length() -i -1)
+            if yield(arr[j],arr[j+1]).positive?
+                arr[j] , arr[j+1] = arr[j+1] , arr[j]
+            end
+        end
+    end
+    print arr
+end
+
 
   x = [4,5,3,7,6,8,96]
   
   print bubble(x)
   
-  
+bubble_sort_by(%w[hi hello hey]) do |left, right|
+    left.length - right.length
+end
