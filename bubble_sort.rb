b29fa08d1ac53abf3368da9cedb0d2063780d412
Precious chiedu <udegbue69@gmail.com>
2020-04-16 10:50:17 +0100

    fixed errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -14,16 +14,22 @@ def bubble(array)
   array
 end
 
-def bubble_sort_by(arr)
-  for i in 1..arr.length()
-    for j in (0..arr.length() - i - 1)
-      if yield(arr[j], arr[j + 1]).positive?
-        arr[j], arr[j + 1] = arr[j + 1], arr[j]
+def bubble_sort_by(array)
+  len_ = array.length - 1
+  loop do
+    sorted = false
+    len_.times do |x|
+      if yield(array[x], array[x + 1]).positive?
+        array[x], array[x + 1] = array[x + 1], array[x]
+        sorted = true
       end
-      end
+    end
+    len_ -= 1
+    break unless sorted
   end
-  print arr
+  print array
 end
+
 
 x = [4, 5, 3, 7, 6, 8, 96]
 print bubble(x)
